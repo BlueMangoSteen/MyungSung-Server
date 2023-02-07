bc418ef7c43e4a616c2e04343adb0f6038985181
Sollertia <sollertia331@gmail.com>
2023-02-07 23:56:33 +0900

    :first_quarter_moon_with_face: init commit

---
+++ src/main/java/com/bluemangosteen/myungsungserver/MyungSungServerApplication.java
@@ -0,0 +1,13 @@
+package com.bluemangosteen.myungsungserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyungSungServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyungSungServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bluemangosteen/myungsungserver/MyungSungServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bluemangosteen.myungsungserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyungSungServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
